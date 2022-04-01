d581d93de134ab15330f5d07f88050c3e809ac52
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-01 14:43:11 -0300

    df

--- service/Microsoft.DSX.ProjectTemplate.Data/DTOs/AuditDto.cs
+++ service/Microsoft.DSX.ProjectTemplate.Data/DTOs/AuditDto.cs
@@ -4,6 +4,7 @@ namespace Microsoft.DSX.ProjectTemplate.Data.DTOs
 {
     public abstract class AuditDto<TType> : BaseDto<TType>
     {
+        // ahora si
         public DateTime CreatedDate { get; set; }
 
         public DateTime UpdatedDate { get; set; }
